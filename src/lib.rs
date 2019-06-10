13014ae90aeebb80464145ec485e8fbb5d117f81
ylxdzsw <ylxdzsw@gmail.com>
2019-06-10 12:16:06 +0800

    .

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
 mod io;
+mod utils;
 
 pub use io::ReadExt;
 impl<R: std::io::Read + ?Sized> ReadExt for R {}
+
+pub use utils::MonadExt;
+impl<T> MonadExt for T {}
+
+pub use utils::SizedMonadExt;
+impl<T: Sized> SizedMonadExt for T {}
