ae7347b4401790b6b28bfce7d0c27635d7bb53c0
Shiwei Zhang <ylxdzsw@gmail.com>
2019-10-22 01:04:41 +0800

    dyn

--- src/structs.rs
+++ src/structs.rs
@@ -1,38 +1,39 @@
 /// a simple exlusively owned pointer that have a destructure callback attached. T can be () for just making callback.
-pub struct RAII<T, F: FnOnce()>(T, Option<F>);
+pub struct RAII<T>(Option<T>, Box<dyn FnOnce(T)>);
 
-impl<T, F: FnOnce()> RAII<T, F> {
-    pub fn new(x: T, callback: F) -> Self {
-        Self(x, Some(callback))
+impl<T> RAII<T> {
+    pub fn new(x: T, callback: impl FnOnce(T) + 'static) -> Self {
+        Self(Some(x), Box::new(callback))
     }
 }
 
-impl<T, F: FnOnce()> Drop for RAII<T, F> {
+impl<T> Drop for RAII<T> {
     fn drop(&mut self) {
-        self.1.take().unwrap()()
+        let drop = std::mem::replace(&mut self.1, Box::new(|_| {}));
+        drop(self.0.take().unwrap())
     }
 }
 
-impl<T, F: FnOnce()> std::ops::Deref for RAII<T, F> {
+impl<T> std::ops::Deref for RAII<T> {
     type Target = T;
     fn deref(&self) -> &T {
-        &self.0
+        self.0.as_ref().unwrap()
     }
 }
 
-impl<T, F: FnOnce()> std::ops::DerefMut for RAII<T, F> {
+impl<T> std::ops::DerefMut for RAII<T> {
     fn deref_mut(&mut self) -> &mut T {
-        &mut self.0
+        self.0.as_mut().unwrap()
     }
 }
 
-impl<T, F: FnOnce()> AsRef<T> for RAII<T, F> {
+impl<T> AsRef<T> for RAII<T> {
     fn as_ref(&self) -> &T {
         self
     }
 }
 
-impl<T, F: FnOnce()> AsMut<T> for RAII<T, F> {
+impl<T> AsMut<T> for RAII<T> {
     fn as_mut(&mut self) -> &mut T {
         self
     }
