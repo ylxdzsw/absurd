cef094a7e6aaa403496d96f7f1f9a9ad9511e979
Shiwei Zhang <ylxdzsw@gmail.com>
2019-10-31 14:53:01 +0800

    .

--- src/mem.rs
+++ src/mem.rs
@@ -2,6 +2,10 @@ pub fn leak<T>(x: T) -> &'static mut T {
     Box::leak(Box::new(x))
 }
 
+pub unsafe fn reclaim<T>(x: *mut T) -> Box<T> {
+    Box::from_raw(x)
+}
+
 pub unsafe fn free<T>(x: *mut T) {
-    Box::from_raw(x);
+    reclaim(x);
 }
