c9553d73d97aec383c5fea2fc0a7e6f9d0c5a3e4
ylxdzsw <ylxdzsw@gmail.com>
2023-12-21 11:09:09 +0000

    Deploying to gh-pages from @ ylxdzsw/absurd@54a4087d8ccd373b614bcb9a60b1b42727828141 🚀

---
+++ implementors/core/convert/trait.From.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"absurd":[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.1/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;T&gt; for <a class=\"struct\" href=\"absurd/struct.ManyTimesCell.html\" title=\"struct absurd::ManyTimesCell\">ManyTimesCell</a>&lt;T&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/core/iter/traits/collect/trait.FromIterator.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"absurd":[["impl&lt;T, S: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.1/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.1/core/iter/traits/collect/trait.FromIterator.html\" title=\"trait core::iter::traits::collect::FromIterator\">FromIterator</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.74.1/std/primitive.tuple.html\">(T, S)</a>&gt; for <a class=\"struct\" href=\"absurd/struct.MinHeap.html\" title=\"struct absurd::MinHeap\">MinHeap</a>&lt;T, S&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
