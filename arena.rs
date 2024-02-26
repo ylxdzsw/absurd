93c1da3f171c6a0d9ef2714d6977c5dfb76b32f3
Shiwei Zhang <ylxdzsw@gmail.com>
2024-02-26 13:12:13 +0800

    .

--- arena.rs
+++ arena.rs
@@ -1,7 +1,5 @@
-use core::mem::MaybeUninit;
+use core::mem::{ManuallyDrop, MaybeUninit};
 use core::cell::UnsafeCell;
-
-use crate::{size_of, align_of};
 
 /// `N` is the initial chunk size (in bytes)
 /// `C` is the maximum number of chunks
@@ -50,21 +48,39 @@ impl<const N: usize, const C: usize> Arena<N, C> {
         unsafe { ptr.assume_init_mut() }
     }
 
-    pub fn alloc_uninitialized<T>(&self) -> &mut MaybeUninit<T> {
-        assert!(size_of!(T) > 0, "Zero sized types are not supported"); // not sure
+    pub fn calloc<T>(&self, value: Vec<T>) -> &mut [T] {
+        let ptr = self.calloc_uninitialized::<T>(value.len());
+        unsafe { core::ptr::copy_nonoverlapping(value.as_ptr(), ptr.as_mut_ptr() as *mut T, value.len()) };
+        let _: Vec<ManuallyDrop<T>> = unsafe { core::mem::transmute(value) };
+        unsafe { core::mem::transmute(ptr) }
+    }
+
+    unsafe fn alloc_layout(&self, layout: core::alloc::Layout) -> *mut u8 {
         loop {
-            let inner = unsafe { &mut *self.inner.get() };
-            let align_offset = inner.ptr.align_offset(align_of!(T));
-            if align_offset + size_of!(T) > inner.capacity {
+            let inner = &mut *self.inner.get();
+            let align_offset = inner.ptr.align_offset(layout.align());
+            if align_offset + layout.size() > inner.capacity {
                 self.grow();
-                continue;
+                continue
             }
 
-            let ptr = unsafe { inner.ptr.add(align_offset) };
-            inner.ptr = unsafe { inner.ptr.add(align_offset + size_of!(T)) };
-            inner.capacity -= align_offset + size_of!(T);
-            return unsafe { &mut *ptr.cast() };
+            let ptr = inner.ptr.add(align_offset);
+            inner.ptr = inner.ptr.add(align_offset + layout.size());
+            inner.capacity -= align_offset + layout.size();
+            return ptr
         }
+    }
+
+    pub fn alloc_uninitialized<T>(&self) -> &mut MaybeUninit<T> {
+        let layout = core::alloc::Layout::new::<T>();
+        let ptr = unsafe { self.alloc_layout(layout) };
+        unsafe { &mut *ptr.cast() }
+    }
+
+    pub fn calloc_uninitialized<T>(&self, count: usize) -> &mut [MaybeUninit<T>] {
+        let layout = core::alloc::Layout::array::<T>(count).unwrap();
+        let ptr = unsafe { self.alloc_layout(layout) };
+        unsafe { core::slice::from_raw_parts_mut(ptr.cast(), count) }
     }
 }
 
@@ -114,6 +130,25 @@ mod tests {
             assert_eq!(x.assume_init(), 42);
             assert_eq!(y.assume_init(), 43);
         }
+    }
+
+    #[test]
+    fn test_arena_calloc() {
+        let arena: Arena = Arena::new();
+        let x = arena.calloc(vec![42, 43]);
+        assert_eq!(x, &[42, 43]);
+    }
+
+    #[test]
+    fn test_arena_calloc_uninitialized() {
+        let arena: Arena = Arena::new();
+        let x = arena.calloc_uninitialized::<bool>(2);
+        let x: &mut [bool] = unsafe {
+            x[0].write(true);
+            x[1].write(false);
+            core::mem::transmute(x)
+        };
+        assert_eq!(x, &[true, false]);
     }
 
     #[test]
