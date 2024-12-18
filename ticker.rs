dfc5298d2c1da9ea8f124948ecbedafa6ec90cba
Shiwei Zhang <ylxdzsw@gmail.com>
2024-12-18 22:53:29 +0800

    .

--- ticker.rs
+++ ticker.rs
@@ -3,22 +3,31 @@
 /// a helper struct to print iteration count and the elapsed time
 #[cfg(feature = "std")]
 pub struct Ticker {
-    iter_count: usize,
     iter_per_print: usize,
+
+    iter_count: usize,
     start_time: std::time::Instant,
+    last_tick: std::time::Instant
 }
 
 #[cfg(feature = "std")]
 impl Ticker {
     pub fn new(iter_per_print: usize) -> Self {
-        Ticker { iter_count: 0, iter_per_print, start_time: std::time::Instant::now() }
+        let now = std::time::Instant::now();
+        Ticker { iter_count: 0, iter_per_print, start_time: now, last_tick: now }
     }
 
-    pub fn tick(&mut self) {
+    pub fn tick(&mut self) -> core::time::Duration {
+        let now = std::time::Instant::now();
+        let elapsed = now - self.last_tick;
+        self.last_tick = now;
+
         self.iter_count += 1;
-        if self.iter_count % self.iter_per_print == 0 {
+        if self.iter_per_print != 0 && self.iter_count % self.iter_per_print == 0 {
             eprintln!("{self}")
         }
+
+        elapsed
     }
 }
 
@@ -37,6 +46,8 @@ impl core::fmt::Display for Ticker {
 #[cfg(feature = "std")]
 impl Drop for Ticker {
     fn drop(&mut self) {
-        eprintln!("{self}")
+        if self.iter_per_print != 0 {
+            eprintln!("{self}")
+        }
     }
 }
